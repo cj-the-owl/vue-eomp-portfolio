3f3181f1638d86b29d0fc9d742539bf37b17b4e5
cj-the-owl <lodewykcarynjayn@gmail.com>
2023-07-03 12:40:26 +0200

    page and router done

--- src/components/navbar-comp.vue
+++ src/components/navbar-comp.vue
@@ -22,7 +22,7 @@
                         <router-link class="nav-link" to="/about">Testimonials</router-link>
                     </li>
                     <li class="nav-item">
-                        <router-link class="nav-link" to="/about">Contact</router-link>
+                        <router-link class="nav-link" to="/contact">Contact</router-link>
                     </li>
                 </ul>
             </div>
