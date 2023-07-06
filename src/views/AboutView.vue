501bbd0055dba00ac88535f4aff24d4cb760c7cf
cj-the-owl <lodewykcarynjayn@gmail.com>
2023-07-06 15:25:59 +0200

    about layout done

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,68 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <div class="p-4 p-md-5 mb-4 text" id="main">
+      <div class="col-md-6 px-0">
+        <h1 class="display-4 fst-italic">
+          Get to know more about me!
+        </h1>
+      </div>
+      <div class="row">
+        <div class="col-md-8">
+          <!-- <h3 class="pb-4 mb-4 fst-italic border-bottom">
+            something
+          </h3> -->
+          <p class="border-top border-dark">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis ex temporibus fuga unde maxime quae nam possimus! Molestiae excepturi, iusto nemo ea nisi consequatur, dignissimos quia perspiciatis a itaque iste!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta officiis obcaecati pariatur praesentium laboriosam quaerat, cupiditate accusamus, nostrum temporibus optio impedit dolorum omnis, similique dolor illum nihil sapiente nesciunt adipisci.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo culpa molestiae deleniti, at veniam alias ipsum magnam voluptate eius error minus porro autem excepturi tenetur odit mollitia repellat pariatur reprehenderit!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates facere ab, unde fugit dolorum repellendus nisi perspiciatis dolorem esse libero praesentium, deleniti nulla quos explicabo amet. Eligendi fugiat obcaecati sed.
+                </p>
+        </div>
+
+        <div class="col-md-4">
+          <div classs="position-sticky" style="top: 2rem">
+            <div class="p-4 mb-3" id="me-card">
+              <img class="fst-italic" src="https://i.postimg.cc/pV5sjZfv/IMG-20230222-141359-139.jpg">
+              <p class="mb-0 mt-3"><img  src="https://i.postimg.cc/bw3Kk60Z/icons8-email-50-1.png">lodewykcarynjayn@gmail.com</p>
+              <p class="mb-0 mt-3"><img src="https://i.postimg.cc/TwRLF3L7/icons8-age-50.png"> 26/07/2000</p>
+              <p class="mb-0 mt-3"><img  src="https://i.postimg.cc/3N1ZVqxp/icons8-location-50.png">Pelicah Heights</p>
+              <p class="mb-0 mt-3"><img  src="https://i.postimg.cc/wMXrvFTT/icons8-phone-50.png">0765297079</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+  .fst-italic {
+    width: 350px;
+    border-radius: 20px;
+  }
+
+  #main {
+    background: rgba(255, 255, 255, 0.2);
+    box-shadow: 0 15px 35px rgba(0, 0, 0, 0.05);
+    backdrop-filter: blur(10px);
+    border: 1px solid rgba(255, 255, 255, 0.25);
+    border-top: 1px solid rgba(255, 255, 255, 0.5);
+    border-left: 1px solid rgba(255, 255, 255, 0.5);
+    border-radius: 20px;
+  }
+
+  #me-card {
+    background: rgba(255, 255, 255, 0.2);
+    box-shadow: 0 15px 35px rgba(0, 0, 0, 0.05);
+    backdrop-filter: blur(10px);
+    border: 1px solid rgba(255, 255, 255, 0.25);
+    border-top: 1px solid rgba(255, 255, 255, 0.5);
+    border-left: 1px solid rgba(255, 255, 255, 0.5);
+    border-radius: 20px;
+  }
+</style>
