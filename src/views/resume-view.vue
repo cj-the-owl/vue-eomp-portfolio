93dba8cdead9ab1c6b6b648982e5003fba7c23e2
cj-the-owl <lodewykcarynjayn@gmail.com>
2023-07-07 11:39:21 +0200

    skills done

--- src/views/resume-view.vue
+++ src/views/resume-view.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="container py-4">
         <div class="row align-items-md-stretch">
+            <h2 class="pt-4">Education & Experience</h2>
             <div v-if="educations" class="col-md-6" id="timeline">
                 <div v-for="education in educations" :key="education.year" class="content">
                     <h4>{{ education.year }}</h4>
@@ -19,6 +20,52 @@
                 </div>
             </div>
             <div v-else>Hang on...</div>
+        </div>
+        <h1>Hard Skills</h1>
+        <div class="hard-skills">
+            <div>
+                <p style="width: 100%" data-value="90">HTML 90%</p>
+                <progress max="100" value="90"></progress>
+            </div>
+            <div>
+                <p style="width: 100%" data-value="90">CSS 90%</p>
+                <progress max="100" value="90"></progress>
+            </div>
+            <div>
+                <p style="width: 100%" data-value="65">JavaScript 65%</p>
+                <progress max="100" value="65"></progress>
+            </div>
+            <div>
+                <p style="width: 100%" data-value="80">Bootstrap 80%</p>
+                <progress max="100" value="80"></progress>
+            </div>
+            <div>
+                <p style="width: 100%" data-value="70">Vue.js 70%</p>
+                <progress max="100" value="70"></progress>
+            </div>
+        </div>
+        <h1 class="mt-3">Soft Skills</h1>
+        <div class="soft-skills">
+            <div>
+                <p style="width: 100%" data-value="80">Communication 85%</p>
+                <progress max="100" value="80"></progress>
+            </div>
+            <div>
+                <p style="width: 100%" data-value="80">Team 90%</p>
+                <progress max="100" value="90"></progress>
+            </div>
+            <div>
+                <p style="width: 100%" data-value="90">Adaptability 90%</p>
+                <progress max="100" value="90"></progress>
+            </div>
+            <div>
+                <p style="width: 100%" data-value="80">Leadership 80%</p>
+                <progress max="100" value="80"></progress>
+            </div>
+            <div>
+                <p style="width: 100%" data-value="80">Problem-solving 80%</p>
+                <progress max="100" value="80"></progress>
+            </div>
         </div>
     </div>
 </template>
@@ -81,5 +128,41 @@
         top: 10px;
         left: -40px;
     }
-    
+    .hard-skills, .soft-skills {
+        background: rgba(255, 255, 255, 0.2);
+        box-shadow: 0 15px 35px rgba(0, 0, 0, 0.05);
+        border-radius: 20px;
+        padding: 5px;
+    }
+
+    #hs-file {
+        color: black !important;
+    }
+    progress[value] {
+        -webkit-appearance: none;
+         appearance: none;
+
+         height: 35px;
+         width: 60rem;
+    }
+    progress[value]::-webkit-progress-bar {
+        background: rgba(255, 255, 255, 0.2);
+        box-shadow: 0 15px 35px rgba(0, 0, 0, 0.05);
+        border-radius: 20px;
+    }
+
+    progress[value]::-webkit-progress-value {
+        background-color:rgba(255, 255, 255, 0.7);
+        border-radius: 20px;
+    }
+
+    progress[value]::-webkit-progress-value::before {
+        content: 'value';
+        position: relative;
+        right: 0;
+        top: -125%;
+        color: black
+}
+
+
 </style>
